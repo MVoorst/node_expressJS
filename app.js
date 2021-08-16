c93e4390fc89fdf8f649766c0b50ea8411f3063f
Marjofijn <marjolijn.vanvoorst@gmail.com>
2021-08-16 10:30:29 +0200

    Express JS introduction

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.get('/favicon.ico', (req, res) => res.sendStatus(204));
+//added this because the browser searches a favicon. This resulted in double execution of the middleware functions below.
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'view', '404.html'))
+
+});
+
+app.listen(3000);
