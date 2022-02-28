d93a2e8f121680c3f133343f3f5065618284e0d4
MVoorst <38377439+MVoorst@users.noreply.github.com>
2022-02-28 21:54:36 +0100

    test

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ app.set('views', 'view');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 const db = require('./util/database');
 
 
@@ -20,21 +27,56 @@ app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(error => console.log(error));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-
-/*
-db.execute('SELECT * FROM products')
-    .then((result) => {
-        console.log(result[0], result[1]);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-*/
-
-
 app.use(errorController.get404);
 
-app.listen(3000);
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Product, { through: CartItem});
+Product.belongsToMany(Cart, { through: CartItem});
+
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem});
+
+
+sequelize.sync()
+    .then(result => {
+        return User.findByPk(1)
+    })
+    .then(user => {
+        if (!user) {
+            User.create({
+                name: 'John Doe',
+                email: 'test@test.com'
+            })
+        }
+        return user;
+    })
+    .then(user => {
+        return user.createCart();
+    })
+    .then(cart => {
+        app.listen(3000)
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+
+
